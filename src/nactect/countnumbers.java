1d8504b75cb1d4f1cfa315658ae90364d4851b5b
SHEBA <sheba17072001@gmail.com>
2023-07-31 11:55:31 +0530

    countnumbers

---
+++ src/nactect/countnumbers.java
@@ -0,0 +1,19 @@
+package nactect;
+
+import java.util.Scanner;
+
+public class countnumbers {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	
+
+				  Scanner scanner = new Scanner(System.in);
+				  System.out.println("enter a string");
+				  String inputString=scanner.nextLine();
+				  int  charactercount= inputString.length();
+				  System.out.println("The total number of characters in the string is:"+charactercount);
+				  
+}
+}
